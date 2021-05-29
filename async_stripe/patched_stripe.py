d9ea948b874285b3f939ca10728d9e209183cf2c
Bharat Chauhan <tell.bhch@gmail.com>
2021-05-29 16:51:18 +0530

    Better way of setting attributes

--- async_stripe/patched_stripe.py
+++ async_stripe/patched_stripe.py
@@ -4,23 +4,13 @@ from .resources import (
     )
 
 class Stripe:
-    def __init__(self):
-        self._api_key = None
-        self._public_key = None
-        self._webhook_secret = None
-
     PaymentMethod = AsyncPaymentMethod
     PaymentIntent = AsyncPaymentIntent
     Customer = AsyncCustomer
 
-    @property
-    def api_key(self):
-        return self._api_key
-
-    @api_key.setter
-    def api_key(self, value):
-        self._api_key = value
-        real_stripe.api_key = value
+    def __setattr__(self, name, value):
+        super().__setattr__(name, value)
+        setattr(real_stripe, name, value)
 
     def __getattr__(self, name):
             return getattr(real_stripe, name)

a7679c5cb6f99bd7078c427b86e326012f429ed2
Bharat Chauhan <tell.bhch@gmail.com>
2022-10-01 10:47:58 +0530

    API updates and tests

--- async_stripe/api_resources/customer.py
+++ async_stripe/api_resources/customer.py
@@ -3,5 +3,5 @@ from async_stripe.api_resources.abstract import patch_nested_resources
 from stripe import util
 
 
-netsted_resources = ['balance_transaction', 'source', 'tax_id']
+netsted_resources = ['balance_transaction', 'cash_balance_transaction', 'source', 'tax_id']
 patch_nested_resources(stripe.Customer, netsted_resources)
--- tests/test_generated_examples.py
+++ tests/test_generated_examples.py
@@ -162,42 +162,6 @@ class TestGeneratedExamples(object):
         await stripe.Invoice.upcoming(customer="cus_9utnxg47pWjV1e")
         request_mock.assert_requested("get", "/v1/invoices/upcoming")
 
-    async def test_order_create(self, request_mock):
-        await stripe.Order.create(
-            description="description",
-            currency="usd",
-            line_items=[{"description": "my line item"}],
-        )
-        request_mock.assert_requested("post", "/v1/orders")
-
-    async def test_order_retrieve(self, request_mock):
-        await stripe.Order.retrieve("order_xyz")
-        request_mock.assert_requested("get", "/v1/orders/order_xyz")
-
-    async def test_order_update(self, request_mock):
-        await stripe.Order.modify(
-            "order_xyz",
-            metadata={"reference_number": "123"},
-            ip_address="0.0.0.0",
-        )
-        request_mock.assert_requested("post", "/v1/orders/order_xyz")
-
-    async def test_order_cancel(self, request_mock):
-        await stripe.Order.cancel("order_xyz")
-        request_mock.assert_requested("post", "/v1/orders/order_xyz/cancel")
-
-    async def test_order_list_line_items(self, request_mock):
-        await stripe.Order.list_line_items("order_xyz")
-        request_mock.assert_requested("get", "/v1/orders/order_xyz/line_items")
-
-    async def test_order_reopen(self, request_mock):
-        await stripe.Order.reopen("order_xyz")
-        request_mock.assert_requested("post", "/v1/orders/order_xyz/reopen")
-
-    async def test_order_submit(self, request_mock):
-        await stripe.Order.submit("order_xyz", expected_total=100)
-        request_mock.assert_requested("post", "/v1/orders/order_xyz/submit")
-
     async def test_paymentintent_create(self, request_mock):
         await stripe.PaymentIntent.create(
             amount=1099,
@@ -1259,10 +1223,6 @@ class TestGeneratedExamples(object):
         request_mock.assert_requested(
             "get", "/v1/mandates/mandate_xxxxxxxxxxxxx"
         )
-
-    async def test_order_list(self, request_mock):
-        await stripe.Order.list(limit=3)
-        request_mock.assert_requested("get", "/v1/orders")
 
     async def test_paymentintent_list(self, request_mock):
         await stripe.PaymentIntent.list(limit=3)

b2a80c0b5d20fb59573eb59d77b0b93de0bb80fa
Bharat Chauhan <tell.bhch@gmail.com>
2022-10-05 09:40:46 +0530

    Remove redundant patch in account.py

--- async_stripe/api_resources/account.py
+++ async_stripe/api_resources/account.py
@@ -14,21 +14,6 @@ async def retrieve_patch(cls, id=None, api_key=None, **params):
 stripe.Account.retrieve = classmethod(retrieve_patch)
 
 
-def serialize_patch(self, previous):
-    params = super(Account, self).serialize(previous)
-    previous = previous or self._previous or {}
-
-    for k, v in six.iteritems(self):
-        if (
-            k == "individual"
-            and isinstance(v, stripe.api_resources.Person)
-            and k not in params
-        ):
-            params[k] = v.serialize(previous.get(k, None))
-
-    return params
-
-
 nested_resources = ["capability", "login_link", "external_account", "person",]
 
 patch_nested_resources(stripe.Account, nested_resources)
